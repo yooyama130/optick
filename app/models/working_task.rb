65932ec4a2a5595bedd50c4819636a98e4c15e88
yooyama130 <yooyama.97.01@gmail.com>
2022-01-08 08:12:55 +0000

    [Add] ルーティング作成

--- app/models/working_task.rb
+++ app/models/working_task.rb
@@ -1,3 +1,4 @@
 class WorkingTask < ApplicationRecord
   belongs_to :user
+  belongs_to :task
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  get 'searches/top'
-  get 'searches/result'
-  get 'working_tasks/index'
-  get 'working_tasks/new'
-  get 'working_tasks/edit'
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/edit'
-  get 'users/show'
-  get 'users/edit'
-  get 'homes/top'
-  get 'homes/about'
+  root to: "homes#top"
+  get "/about", to: "homes#about"
   devise_for :users
+  resources :users, only: [:show, :edit] do
+    resources :tasks, except: [:show]
+    # indexのみ、:date（日付の情報）をURLから送るため、別に書いている
+    get "working_tasks/:date", to: "working_tasks#index", as: "working_tasks"
+    resources :working_tasks, except: [:index,:show]
+    # 検索機能用
+    get "working_tasks/search", to: "searches#top", as: "search"
+    get "working_tasks/search/result", to: "searches#result", as: "search_result"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
