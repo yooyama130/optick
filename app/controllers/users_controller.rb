566077b66e95ce82f7ccd3039017941109ca744a
yooyama130 <yooyama.97.01@gmail.com>
2022-01-11 02:05:17 +0000

    [Update] ユーザープロフィール編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy_image
+    user = User.find(params[:id])
+    # 画像削除の方法は、画像idに nil を代入する
+    user.update(profile_image_id: nil)
+    redirect_to edit_user_path(user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :profile_image, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/about", to: "homes#about"
   devise_for :users
-  resources :users, only: [:show, :edit] do
+  resources :users, only: [:show, :edit, :update] do
     resources :tasks, except: [:show]
     delete "tasks/:id/destroy_icon", to: "tasks#destroy_icon", as: "task_destroy_icon"
     # indexのみ、:date（日付の情報）をURLから送るため、別に書いている
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
     get "working_tasks/search", to: "searches#top", as: "search"
     get "working_tasks/search/result", to: "searches#result", as: "search_result"
   end
+  delete "users/:id/destroy_icon", to: "users#destroy_image", as: "user_destroy_image"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
