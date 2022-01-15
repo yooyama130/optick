62dc481023b57976afe2f23ca44ed79e8e0ec77d
yooyama130 <yooyama.97.01@gmail.com>
2022-01-15 08:24:49 +0000

    [Update] カレンダーのタグ付けされた日にマークを表示するように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,18 +10,23 @@ class UsersController < ApplicationController
     @todays_working_tasks = @user.working_tasks.where(being_measured?: false, started_at: today.all_day)
     # 今日のタスクを、task_idごとに、データをまとめる(3種類あれば3つにする、5種類あれば5つにする)
     @todays_working_tasks_grouped = @todays_working_tasks.group(:task_id)
+
     # 計測中のタスクを表示する
     @working_tasks_measuring = @user.working_tasks.where(being_measured?: true)
-    # カレンダー用
-    @working_tasks_all = @user.working_tasks.all
 
-    # グラフに送信するためのデータ生成
+    # カレンダー用 ここから----------------------------------------------------------------
+    @my_working_tasks = @user.working_tasks.all
+    @my_events = @user.events.all
+    # カレンダー用 ここまで----------------------------------------------------------------
+
+    # グラフに送信するためのデータ生成 ここから--------------------------------------------
     gon.data_of_tasks = []
     gon.data_of_working_time_sums = []
     @todays_working_tasks_grouped.each do |working_task|
       gon.data_of_tasks << working_task.task.content
       gon.data_of_working_time_sums << @todays_working_tasks.where(task_id: working_task.task_id).pluck(:working_time).sum
     end
+    # グラフに送信するためのデータ生成 ここまで--------------------------------------------
   end
 
   def edit
