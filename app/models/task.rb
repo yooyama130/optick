20627214ba0b7482bfeb29ccadf2d42966d3812e
yooyama130 <yooyama.97.01@gmail.com>
2022-01-19 03:00:24 +0000

    [Fix] タスク計測のレイアウトやtaskのdependent: :destroy設定を追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  has_many :working_tasks
+  has_many :working_tasks, dependent: :destroy
   attachment :icon_image
 
   # uniqueness: { scope: :user } = 一人のユーザーがタスクの名前に同じものを使えない
