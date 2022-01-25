6bfe818c43639654c423f80eb4e6f0a44d246947
yooyama130 <yooyama.97.01@gmail.com>
2022-01-25 03:39:07 +0000

    [Fix] working_taskの実働時間が24時間を超えても正しく表示されるように修正

--- app/models/working_task.rb
+++ app/models/working_task.rb
@@ -10,6 +10,37 @@ class WorkingTask < ApplicationRecord
     being_measured? == false
   end
 
+  # ------------------------workingtimeを数値化するのに使用（ここから）-------------------------------
+  # 1桁を2桁にする（stringとして扱う)
+  def zero_for_digits(int)
+    if int < 10
+      return "0"+ int.to_s
+    end
+    int.to_s
+  end
+
+  # 数値（working_time）を時間化
+  def int_to_time(int)
+    hour = 0
+    min = 0
+    sec = 0
+    while int >= 3600 do
+      # 1時間 = 3600秒 ---> 3600を引いた回数を時間hourとして出す
+      int  -= 3600
+      hour += 1
+    end
+    while int >= 60 do
+      # 1分 = 60秒 ---> 60を引いた回数を分minとして出す
+      int  -= 60
+      min += 1
+    end
+    # 残った時間を秒secとして出す
+    sec = int
+    # 最後に文字化
+    "#{zero_for_digits(hour)}:#{zero_for_digits(min)}:#{zero_for_digits(sec)}"
+  end
+  # ------------------------workingtimeを数値化するのに使用（ここまで）-------------------------------
+
   # 『記録済みタスクを調べる』機能に使う
   def self.search(container, date_range_start, date_range_end, events, task)
     # 1.最初にstarted_atが検索フォームから送られてきた日付範囲に入っているかを検索。
