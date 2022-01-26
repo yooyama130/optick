a050928e9f87e5b98b5e2c70c95caa0668516bb9
yooyama130 <yooyama.97.01@gmail.com>
2022-01-26 02:38:32 +0000

    [Fix] ハンバーガメニューの色を修正 & 言語切替をダブルクリックしたら「繰り返しリダイレクトされている」エラーの修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,8 +5,14 @@ class HomesController < ApplicationController
   def about
   end
 
+  # 代入をせず、前回代入したもの保持したいケースもあるのでメソッドの外でクラス変数として定義
+  @@prev_url = nil
   def change_language
-   session[:locale] = params[:language]
-   redirect_back(fallback_location: root_path)
+    # ダブルクリックすると『リダイレクトが繰り返されます』となるので、
+    # （原因：ダブルクリックする　＝　直前のURLがchange_languageとみなされ、無限ループ）
+    # 直前のURLがこのアクションでなければ、クラス変数prev_urlに代入するようにする
+    @@prev_url = request.referer unless request.referer.include?("/change_language/")
+    session[:locale] = params[:language]
+    redirect_to @@prev_url
   end
 end
